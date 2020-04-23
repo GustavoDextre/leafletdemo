e639ef3dd07981cf0c657332cfc198bd51e6fbc9
unknown <gustavodextrezubieta@gmail.com>
2020-04-22 19:14:52 -0500

    first commit

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,26 +1,61 @@
-var map = L.map('map-template').setView([51.505, -0.09], 3);
+var map = L.map('map-template').setView([-12.0630149, -77.0296179], 10);
 
-const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
-const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+const openStreetURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const stamenTonerURL = 'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png';
+const darkMatterURL = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png';
 
-const tile = L.tileLayer(tileURL2);
+var tile = L.tileLayer(openStreetURL);
+
+function selectMap(){
+
+    var mappingValue = document.getElementById('mapping').value;
+
+    if(mappingValue == 1){
+        tile = L.tileLayer(openStreetURL);
+        map.addLayer(tile);
+    }
+    if(mappingValue == 2){
+        tile = L.tileLayer(stamenTonerURL);
+        map.addLayer(tile);
+    }
+    if(mappingValue == 3){
+        tile = L.tileLayer(darkMatterURL);
+        map.addLayer(tile);
+    }
+}
+
+var texto = 'Usted está aquí'
 
 // Socket Io
 const socket = io.connect();
 
+const user = () => {
+  Swal.fire({
+      title: "Ingrese su nombre por favor",
+      input: 'text',
+      showCancelButton: true        
+  }).then((result) => {
+      if (result.value) {
+          Swal.fire(
+              `Bienvenido ${result.value}`
+            )
+      }
+  });
+}
+
 // Marker
-const marker = L.marker([50.5, 30.5]); // kiev, ukraine
+/* const marker = L.marker([-12.0630149, -77.0296179]); // kiev, ukraine
 marker.bindPopup('Hello There!');
-map.addLayer(marker);
+map.addLayer(marker); */
 
 // Geolocation
-map.locate({enableHighAccuracy: true})
+map.locate({enableHighAccuracy: true});
 map.on('locationfound', (e) => {
-  const coords = [e.latlng.lat, e.latlng.lng];
-  const newMarker = L.marker(coords);
-  newMarker.bindPopup('You are Here!');
-  map.addLayer(newMarker);
-  socket.emit('userCoordinates', e.latlng);
+const coords = [e.latlng.lat, e.latlng.lng];
+const newMarker = L.marker(coords);
+newMarker.bindPopup(texto);
+map.addLayer(newMarker);
+socket.emit('userCoordinates', e.latlng);
 });
 
 // socket new User connected
